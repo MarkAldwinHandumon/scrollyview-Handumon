515acca3d18d1d7b5f1daefdf5a6693ff49b248f
Mark Aldwin Handumon <handumonmark123@gmail.com>
2023-11-19 20:40:33 +0800

    scrollview

---
+++ src/User.jsx
@@ -0,0 +1,83 @@
+export const users = [
+    {
+      id: 1,
+      name: {
+        firstname: "CHARIZ MAE",
+        middlename: "T",
+        lastname: "BACO",
+      },
+      address: "Los Angeles, California",
+    },
+    {
+      id: 2,
+      name: {
+        firstname: "KIM",
+        middlename: "T",
+        lastname: "COMEGHOD",
+      },
+      address: "Las Vegas, Nevada",
+    },
+    {
+      id: 3,
+      name: {
+        firstname: "MA LIZA",
+        middlename: "",
+        lastname: "DAQUIS",
+      },
+      address: "Paris, France",
+    },
+    {
+      id: 4,
+      name: {
+        firstname: "JAMES",
+        middlename: "",
+        lastname: "ESTOCADO",
+      },
+      address: "Los Angeles, California",
+    },
+    {
+      id: 5,
+      name: {
+        firstname: "DESIREE",
+        middlename: "",
+        lastname: "GUMONIT",
+      },
+      address: "Los Angeles, California",
+    },
+    {
+      id: 6,
+      name: {
+        firstname: "MARK ALDWIN",
+        middlename: "C",
+        lastname: "HANDUMON",
+      },
+      address: "Las Vegas, Nevada",
+    },
+    {
+      id: 7,
+      name: {
+        firstname: "LAIZA",
+        middlename: "",
+        lastname: "HANGAD",
+      },
+      address: "Paris, France",
+    },
+    {
+      id: 8,
+      name: {
+        firstname: "MARY",
+        middlename: "",
+        lastname: "HANDUMON",
+      },
+      address: "Los Angeles, California",
+    },
+    {
+      id: 9,
+      name: {
+        firstname: "CHERISH",
+        middlename: "",
+        lastname: "INDOYON",
+      },
+      address: "Los Angeles, California",
+    },
+  ];
